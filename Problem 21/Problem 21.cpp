f9c6dd4dca04e25c4c1f9f451de9bba7a728e3f4
ramywael <ramybakry9@gmail.com>
2025-02-10 23:15:35 +0200

    Solve Problem-21

---
+++ Problem 21/Problem 21.cpp
@@ -0,0 +1,31 @@
+
+/*
+ Write A program to print a fibonacci series of 10
+*/
+
+#include <iostream>
+using namespace std;
+
+
+void WriteFibonacci(int length) {
+    int firstVal=1;
+    int secondVal=0;
+    int febNumber=0;
+    cout << "1"<<" ";
+    for (int i = 2;i <=length;i++) {
+        febNumber = firstVal + secondVal;
+        cout << febNumber << " ";
+        secondVal =firstVal  ;
+        firstVal = febNumber;
+       
+    }
+}
+
+
+
+int main()
+{
+    WriteFibonacci(15);
+    system("pause>0");
+}
+
