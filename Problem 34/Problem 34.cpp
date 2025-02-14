8c7d403ba49649a8462c1c0872d20553f27f2153
ramywael <ramybakry9@gmail.com>
2025-02-14 19:22:59 +0200

    Solve Problem-34

---
+++ Problem 34/Problem 34.cpp
@@ -0,0 +1,51 @@
+/*
+ Write a program to read a string then print all vowels in that string
+( Vowels are a,e,i,o,u )
+*/
+
+#include <iostream>
+#include<string>
+#include <iomanip>
+using namespace std;
+
+string ReadString() {
+	string inputString;
+	cout << "Please Enter Your String ? : \n";
+	getline(cin, inputString);
+	return inputString;
+
+}
+
+
+bool isVowel(char arr[5], char specificCharacter) {
+	specificCharacter = tolower(specificCharacter);
+	for (int i = 0;i < 5;i++) {
+		if (arr[i] == specificCharacter)
+			return true;
+	}
+	return false;
+}
+
+void PrintVowelsOnString(char arr[5], string str) {
+	for (int i = 0;i < str.size();i++) {
+		if (isVowel(arr, str[i])) {
+			cout << setw(3) << str[i];
+		}
+	}
+}
+
+int main()
+{
+	string str = ReadString();
+	char arr[5] = { 'a','e','i','o','u' };
+
+	cout << "Vowels In String are :";
+	PrintVowelsOnString(arr, str);
+
+
+
+	system("pause>0");
+
+
+}
+
