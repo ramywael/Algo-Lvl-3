ce2b772b278d335be4a5cea50b2c102911da9444
ramywael <ramybakry9@gmail.com>
2025-02-14 21:33:00 +0200

    Solve Problem-37.1

--- Problem 37/Problem 37.cpp
+++ Problem 37/Problem 37.cpp
@@ -1,20 +1,54 @@
-// Problem 37.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ Write a program to read a string then count each Word in that string
+*/
 
 #include <iostream>
+#include<string>
+#include <iomanip>
+#include<vector>
+using namespace std;
+
+string ReadString() {
+	string inputString;
+	cout << "Please Enter Your String ? : \n";
+	getline(cin, inputString);
+	return inputString;
+
+}
+
+vector<string> CountEachWordInString(string str, string delimiter) {
+	int pos = 0;
+	string sWord;
+	vector<string> splitVector;
+	short count = 0;
+	while ((pos = str.find(delimiter)) != std::string::npos) {
+		sWord = str.substr(0, pos);
+		if (sWord != "") {
+			splitVector.push_back(sWord);
+		}
+		str.erase(0, pos + delimiter.length());
+	}
+	if (str != "") {
+		splitVector.push_back(str);
+	}
+	return splitVector;
+}
+
+void PrintWachWordInVector(vector<string>& splitVector) {
+	cout <<"Tokens :" << splitVector.size()<<"\n";
+	for (string& word : splitVector) {
+		cout << word << "\n";
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string str = ReadString();
+  vector<string> splitVeector=CountEachWordInString(str, " ");
+  PrintWachWordInVector(splitVeector);
+
+	system("pause>0");
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
